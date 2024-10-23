e750464684b24ea6b920d6c6cc2dce47291a22ac
Conner Swenberg <conner.swenberg@coinbase.com>
2024-10-23 11:39:15 -0700

    Add SpendPermissionManager deployment

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.20;
 import {Script, console2} from "forge-std/Script.sol";
 import {Strings} from "openzeppelin-contracts/contracts/utils/Strings.sol";
 
+import {SpendPermissionManager} from "../src/SpendPermissionManager.sol";
+
 /**
  * forge script Deploy --account dev --sender $SENDER --rpc-url $BASE_SEPOLIA_RPC --verify --verifier-url
  * $SEPOLIA_BASESCAN_API --etherscan-api-key $BASESCAN_API_KEY --broadcast -vvvv
@@ -19,7 +21,9 @@ contract Deploy is Script {
         vm.stopBroadcast();
     }
 
-    function deploy() internal {}
+    function deploy() internal {
+        new SpendPermissionManager{salt: 0}();
+    }
 
     function logAddress(string memory name, address addr) internal pure {
         console2.logString(string.concat(name, ": ", Strings.toHexString(addr)));
