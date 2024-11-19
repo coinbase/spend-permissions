d4a72c48d65ffa1541bb95c46a6cf170fce12efd
Amie Corso <amie.corso@coinbase.com>
2024-11-18 19:20:39 -0800

    add tests for isApproved and isRevoked getters

---
+++ test/src/SpendPermissions/isApproved.t.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {SpendPermissionManager} from "../../../src/SpendPermissionManager.sol";
+
+import {SpendPermissionManagerBase} from "../../base/SpendPermissionManagerBase.sol";
+
+contract IsApprovedTest is SpendPermissionManagerBase {
+    function setUp() public {
+        _initializeSpendPermissionManager();
+    }
+
+    function test_isApproved_true(
+        address account,
+        address spender,
+        uint48 start,
+        uint48 end,
+        uint48 period,
+        uint160 allowance,
+        uint256 salt,
+        bytes memory extraData
+    ) public {
+        vm.assume(spender != address(0));
+        vm.assume(start < end);
+        vm.assume(period > 0);
+        vm.assume(allowance > 0);
+
+        SpendPermissionManager.SpendPermission memory spendPermission = SpendPermissionManager.SpendPermission({
+            account: account,
+            spender: spender,
+            token: NATIVE_TOKEN,
+            start: start,
+            end: end,
+            period: period,
+            allowance: allowance,
+            salt: salt,
+            extraData: extraData
+        });
+        vm.prank(account);
+        mockSpendPermissionManager.approve(spendPermission);
+        vm.assertTrue(mockSpendPermissionManager.isValid(spendPermission));
+        vm.assertTrue(mockSpendPermissionManager.isApproved(mockSpendPermissionManager.getHash(spendPermission)));
+    }
+
+    function test_isApproved_false(
+        address account,
+        address spender,
+        uint48 start,
+        uint48 end,
+        uint48 period,
+        uint160 allowance,
+        uint256 salt,
+        bytes memory extraData
+    ) public {
+        vm.assume(spender != address(0));
+        vm.assume(start < end);
+        vm.assume(period > 0);
+        vm.assume(allowance > 0);
+
+        SpendPermissionManager.SpendPermission memory spendPermission = SpendPermissionManager.SpendPermission({
+            account: account,
+            spender: spender,
+            token: NATIVE_TOKEN,
+            start: start,
+            end: end,
+            period: period,
+            allowance: allowance,
+            salt: salt,
+            extraData: extraData
+        });
+        // no approval
+        vm.assertFalse(mockSpendPermissionManager.isValid(spendPermission));
+        vm.assertFalse(mockSpendPermissionManager.isApproved(mockSpendPermissionManager.getHash(spendPermission)));
+    }
+}
---
+++ test/src/SpendPermissions/isRevoked.t.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {SpendPermissionManager} from "../../../src/SpendPermissionManager.sol";
+
+import {SpendPermissionManagerBase} from "../../base/SpendPermissionManagerBase.sol";
+
+contract IsRevokedTest is SpendPermissionManagerBase {
+    function setUp() public {
+        _initializeSpendPermissionManager();
+    }
+
+    function test_isRevoked_true(
+        address account,
+        address spender,
+        uint48 start,
+        uint48 end,
+        uint48 period,
+        uint160 allowance,
+        uint256 salt,
+        bytes memory extraData
+    ) public {
+        vm.assume(spender != address(0));
+        vm.assume(start < end);
+        vm.assume(period > 0);
+        vm.assume(allowance > 0);
+
+        SpendPermissionManager.SpendPermission memory spendPermission = SpendPermissionManager.SpendPermission({
+            account: account,
+            spender: spender,
+            token: NATIVE_TOKEN,
+            start: start,
+            end: end,
+            period: period,
+            allowance: allowance,
+            salt: salt,
+            extraData: extraData
+        });
+        vm.startPrank(account);
+        mockSpendPermissionManager.approve(spendPermission);
+        vm.assertTrue(mockSpendPermissionManager.isValid(spendPermission));
+        mockSpendPermissionManager.revoke(spendPermission);
+        vm.assertTrue(mockSpendPermissionManager.isRevoked(mockSpendPermissionManager.getHash(spendPermission)));
+    }
+
+    function test_isRevoked_false(
+        address account,
+        address spender,
+        uint48 start,
+        uint48 end,
+        uint48 period,
+        uint160 allowance,
+        uint256 salt,
+        bytes memory extraData
+    ) public {
+        vm.assume(spender != address(0));
+        vm.assume(start < end);
+        vm.assume(period > 0);
+        vm.assume(allowance > 0);
+
+        SpendPermissionManager.SpendPermission memory spendPermission = SpendPermissionManager.SpendPermission({
+            account: account,
+            spender: spender,
+            token: NATIVE_TOKEN,
+            start: start,
+            end: end,
+            period: period,
+            allowance: allowance,
+            salt: salt,
+            extraData: extraData
+        });
+        // no approval
+        vm.assertFalse(mockSpendPermissionManager.isValid(spendPermission));
+        vm.prank(account);
+        mockSpendPermissionManager.revoke(spendPermission);
+        vm.assertTrue(mockSpendPermissionManager.isRevoked(mockSpendPermissionManager.getHash(spendPermission)));
+    }
+}
